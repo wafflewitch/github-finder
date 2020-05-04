f0a62979db290871b92840fc867c598aa4e36c49
wafflewitch <jessicapruitt717@gmail.com>
2020-05-04 13:00:32 -0500

    Refactor some component classes into functions.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
-// import UserItem from './components/users/UserItem';
 import './App.css';
 
 class App extends Component {
